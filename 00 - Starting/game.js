b33c02c996872aa69bf4811bc1cfbccbbf85ae6e
Nicolás Proto <nicolasproto@gmail.com>
2019-05-07 22:15:29 -0300

    Starting setup

---
+++ 00 - Starting/game.js
@@ -0,0 +1,14 @@
+// create a new scene
+
+let gameScene = new Phaser.Scene('Game');
+
+// set the configuration of the game
+let config = {
+    type: Phaser.AUTO, //Phaser will use WebGL if available
+    width: 640,
+    height: 360,
+    scene: gameScene
+};
+
+// create a new game, pass the configuration
+let game = new Phaser.Game(config);
