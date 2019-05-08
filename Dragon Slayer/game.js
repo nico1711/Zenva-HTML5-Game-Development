f2a546547dffbb65c834f6a42e76b076caa4bc91
Nicolás Proto <nicolasproto@gmail.com>
2019-05-08 16:01:30 -0300

    Added goal and overlap script

--- Dragon Slayer/game.js
+++ Dragon Slayer/game.js
@@ -1,5 +1,4 @@
 // create a new scene
-
 let gameScene = new Phaser.Scene('Game');
 
 // initiate scene parameters
@@ -13,21 +12,25 @@ gameScene.preload = function(){
     this.load.image('background', 'assets/background.png')
     this.load.image('player', 'assets/player.png')
     this.load.image('enemy', 'assets/dragon.png')
+    this.load.image('goal', 'assets/treasure.png')
 };
 
 // called once after the preload ends
 gameScene.create = function() {
     // create bg sprite
     this.bg = this.add.sprite(0, 0, 'background');
-    
     // change the origin to the top-left corner
     this.bg.setOrigin(0,0);
     
     // create the player
-    this.player = this.add.sprite(40, 180, 'player');
-    
+    this.player = this.add.sprite(40, this.sys.game.config.height / 2, 'player');
     // with setScale you can change the scale of an image
     this.player.setScale(0.5);
+    
+    // create the treasure
+    this.goal = this.add.sprite(this.sys.game.config.width - 80, this.sys.game.config.height / 2, 'goal');
+    // scale it to half size
+    this.goal.setScale(0.5);
     
 //    // create an enemy1
 //    this.enemy1 = this.add.sprite(250, 180, 'enemy');
@@ -40,14 +43,29 @@ gameScene.create = function() {
 //    this.enemy2.setScale(0.75);
 //    // flip
 //    this.enemy2.flipX = true;
-//    
+//
+    
 };
 
 // this is called up to 60 times per second
 gameScene.update = function(){
+    // checks for input
     if(this.input.activePointer.isDown){
         // player walks
         this.player.x += this.playerSpeed;
+    };
+    
+    // variables to check treasure overlap
+    let playerRect = this.player.getBounds();
+    let treasureRect = this.goal.getBounds();
+    
+    // treasure overlap check
+    if(Phaser.Geom.Intersects.RectangleToRectangle(playerRect, treasureRect)) {
+        console.log('reached goal!');
+        
+        // restarts the Scene
+        this.scene.restart();
+        return;
     };
     
 };
